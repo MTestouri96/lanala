26b782b55a99d58715ac4ac2731a75eb28a15467
MTestouri96 <MTestouri96@users.noreply.github.com>
2019-02-12 16:46:17 +0100

    KeyBorad

--- WorkSpace/Screens/LoginScreen.js
+++ WorkSpace/Screens/LoginScreen.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {TextInput, Text, Image, ScrollView, View, Button, KeyboardAvoidingView} from 'react-native';
+import {TextInput, Text, Image, View, Button, KeyboardAvoidingView} from 'react-native';
 //import {Button} from 'react-native-elements';
 import StylesCss from '../StylesCSS/StylesCss'
 
@@ -12,13 +12,15 @@ class LoginScreen extends Component {
 
   render() {   
     return (
-      <KeyboardAvoidingView backgroundColor="#FCFCFC" style={StylesCss.container} >
+      <KeyboardAvoidingView behavior ='padding' enabled='true' /*backgroundColor="#FCFCFC" style={StylesCss.container}*/  >
         <Image source={require('../Ressource/home.png')}/> 
+      
         <TextInput placeholder= "Username"
                   onChangeText={(text) => this.setState({text})}/>
         <TextInput placeholder= "Password"
                   secureTextEntry={true}
                   onChangeText={(text) => this.setState({text})}/>
+      
         <Text>Request new password.</Text>
         <View style={StylesCss.containerbtn}>
           <Button raised
@@ -27,7 +29,7 @@ class LoginScreen extends Component {
               onPress={() => (this.props.navigation.navigate('HomeScreen'))}
               title="LOGIN"/>
         </View>
-      </KeyboardAvoidingView>
+        </KeyboardAvoidingView>
     );
   };
 }
